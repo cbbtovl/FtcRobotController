2e2ba568146181a62b115760b76045c55268fe28
Intern Client <leminh0932712005@gmail.com>
2025-12-22 20:04:35 +0700

    Create HelloWorld.java
    my first programe

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class HelloWorld extends OpMode {
+
+
+    @Override
+    public void init() {
+
+        telemetry.addData("Hello", "LeMinh");
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
